7648aa6e497498882d5ffe4bee4617e3cafda2d0
Juan Gerardo <jmendez1025@cue.edu.co>
2024-12-19 22:47:26 -0500

    feat: hero cta implemented

--- src/features/hero/hero-cta/HeroCta.tsx
+++ src/features/hero/hero-cta/HeroCta.tsx
@@ -1,10 +1,21 @@
+import { useTranslation } from 'react-i18next';
+import { Button } from '@components/atoms';
+import { BsMailbox } from 'react-icons/bs';
+import { FaLinkedin } from 'react-icons/fa';
 import './hero-cta.scss';
+
 function HeroCta() {
+  const { t } = useTranslation();
   return (
-    <div className='hero-cta'>
-        
+    <div className="hero-cta">
+      <Button variant="secondary" size="md" icon={<BsMailbox />}>
+        <a href="#a"> {t('hero.contact')}</a>
+      </Button>
+      <Button variant="secondary" size="md" icon={<FaLinkedin />}>
+        <a href="https://www.linkedin.com/in/juan-gerardo-m-l/" target='_blank'> {t('hero.linkedin')}</a>
+      </Button>
     </div>
-  )
+  );
 }
 
-export default HeroCta
+export default HeroCta;
--- src/features/hero/hero-header/HeroHeader.tsx
+++ src/features/hero/hero-header/HeroHeader.tsx
@@ -1,21 +1,23 @@
 import { useTranslation } from 'react-i18next';
-import {Heading} from '@components/atoms';
+import { Heading} from '@components/atoms';
 import { Header } from '@components/molecules';
+
 import './hero-header.scss';
+import HeroCta from '../hero-cta/HeroCta';
 
 function HeroHeader() {
   const { t } = useTranslation();
   return (
-    <Header className='hero-header'>
+    <Header className="hero-header">
       <Heading type="h1" size="xl">
-        {t('hero.title')} <span className='hero-header__name'>{t('hero.name')}</span>
+        {t('hero.title')}{' '}
+        <span className="hero-header__name">{t('hero.name')}</span>
       </Heading>
       <Heading type="h2" size="xl">
         {t('hero.subtitle')}
       </Heading>
-      <p className='hero-header__description'>
-        {t('hero.description')}
-      </p>
+      <p className="hero-header__description">{t('hero.description')}</p>
+      <HeroCta />
     </Header>
   );
 }
