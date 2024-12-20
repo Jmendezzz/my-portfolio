1d4cfa3a5f048fd58fe26b453e792dfbc9579f62
Juan Gerardo <jmendez1025@cue.edu.co>
2024-12-19 22:46:48 -0500

    feat: button implemented

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,5 +2,6 @@ import Logo from "./logo/Logo";
 import Link from "./link/Link";
 import Heading from "./heading/Heading";
 import Section from "./section/Section";
+import Button from "./button/Button";
 
-export { Logo, Link, Heading, Section };
+export { Logo, Link, Heading, Section, Button };
