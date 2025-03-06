686f305604a831e8af936c9add04bf0261a51a87
Juan Gerardo <jmendez1025@cue.edu.co>
2025-03-05 20:02:54 -0500

    styles: change  styles on scroll

--- src/components/organisms/nav-bar/NavBar.tsx
+++ src/components/organisms/nav-bar/NavBar.tsx
@@ -2,19 +2,27 @@ import { Logo, Link } from '@components/atoms';
 import { LanguageSwitcher } from '@components/molecules';
 import { useTranslation } from 'react-i18next';
 import './nav-bar.scss';
+import { useScrollPosition } from '@/hooks/useScrollPosition';
+
+const SCROLL_THRESHOLD = 50;
 
 function NavBar() {
   const { t } = useTranslation();
+  const isScrolled = useScrollPosition(SCROLL_THRESHOLD);
+
   return (
-    <nav className="nav">
-      <Logo />
-      <div className='nav__right'>
-        <ul className="nav__list">
-          <Link to="#projects">{t('nav.projects')}</Link>
-          <Link to="#">{t('nav.contact')}</Link>
-        </ul>
-        <LanguageSwitcher />
+    <nav className={`nav ${isScrolled ? 'nav--scrolled' : ''}`}>
+      <div className='nav__content'>
+        <Logo size={`${isScrolled ? 'small' : 'medium'}`}/>
+        <div className='nav__right'>
+          <ul className="nav__list">
+            <Link size={`${isScrolled ? 'small' : 'medium'}`} to="#projects">{t('nav.projects')}</Link>
+            <Link size={`${isScrolled ? 'small' : 'medium'}`}  to="mailto:juange.mendez.lopez@gmail.com">{t('nav.contact')}</Link>
+          </ul>
+          <LanguageSwitcher />
+        </div>
       </div>
+
     </nav>
   );
 }
